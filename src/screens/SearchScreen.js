3e8037518c505effef1d2375db253604375d7271
Kyle Ennis <kyleennis@Kyles-MacBook-Pro.local>
2020-01-07 23:35:20 -0800

    google map added

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 //import constyles from '../styles/constyles';
 //import colors from '../styles/colors';
 import { Button, Row, Container, Col, Form } from 'react-bootstrap';
+import GoogleMapReact from 'google-map-react';
+
 
 
 class SearchScreen extends Component {
@@ -26,8 +28,36 @@ class SearchScreen extends Component {
     }
 
     render() {
-        return <Container>
-            {this.props.user.username}
+        return <Container fluid>
+            <Row>
+
+            
+            <Col>
+roo
+            </Col>
+
+            <Col xs={10} fluid style = {{height:"100vh", backgroundColor : 'blue'}}>
+                <div style={{height : '5%'}}/>
+            <div style={
+                //{width : '400px', height : '400px'}
+                {width : '100%', height : '90%'}
+                }>
+                    <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyB-N2bitfPMd84UXUTcAgIF0F4VICsWoGs' }}
+                    defaultCenter={{
+                        lat: 45.523208,
+                        lng: -122.689243
+                    }}
+                    defaultZoom={11}
+                    ></GoogleMapReact>
+            </div>
+            <div style={{height : '5%'}}/>
+            </Col>
+            
+            <Col>
+            bar
+            </Col>
+            </Row>
         </Container>
     };
 }
